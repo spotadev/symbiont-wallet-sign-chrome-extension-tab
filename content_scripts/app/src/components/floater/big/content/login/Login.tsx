9ff893d32ebdc83411284e206bbddce19dcc0b9f
javaspeak <john.charles.dickerson@gmail.com>
2023-09-28 14:23:15 +0300

    master
    Implementing Login.tsx.  Clicking on buttons there will
    open connecting / disconnecting in a new browser
    extension tab

--- content_scripts/app/src/components/floater/big/content/login/Login.tsx
+++ content_scripts/app/src/components/floater/big/content/login/Login.tsx
@@ -10,17 +10,102 @@ export default function Login(props: any) {
     setLoggedIn,
   } = useGlobalStateContext();
 
-  return (
-    <div className={appStyle.container}>
-      <div className={appStyle.title}>Login</div>
-      <div className={appStyle.row}>
-        <div className={appStyle.col_25}>
-          Blah
+  const [connected, setConnected] = useState(false);
+  const [walletAddress, setWalletAddress] = useState(false);
+
+  const extensionHash = chrome.runtime.id;
+  const chromeExtensionUrl = `chrome-extension://{extensionHash}`;
+
+  const openWalletConnectInNewTab = () => {
+
+    chrome.runtime.sendMessage({ action: "openWalletCollectInNewTab" });
+  }
+
+  const getJSX = () => {
+    if (connected) {
+      return getConnectedJSX();
+    }
+    else {
+      return getConnectJSX();
+    }
+  }
+
+  const disconnect = () => {
+    chrome.runtime.sendMessage({ action: "openDisconnectInNewTab" });
+  }
+
+  const getConnectedJSX = () => {
+    return (
+      <div className={appStyle.container}>
+        <div className={appStyle.title}>Connected</div>
+        <div className={appStyle.row}>
+          <div className={appStyle.col_25}>
+            Wallet Address:
+          </div>
+          <div className={appStyle.col_75}>
+            {walletAddress}
+          </div>
         </div>
-        <div className={appStyle.col_75}>
-          Bling
+        <div className={appStyle.row}>
+          <div className={appStyle.col_100}>
+            To disconnect click the Disconnect button.  It will open up a new tab with
+            a button to disconnect.
+          </div>
+        </div>
+        <div className={appStyle.row}>
+          <div className={appStyle.col_100}>
+            <button
+              className={appStyle.button}
+              onClick={disconnect}>Disconnect</button>
+          </div>
         </div>
       </div>
-    </div>
+    );
+  }
+
+  const getConnectJSX = () => {
+    return (
+      <div className={appStyle.container}>
+        <div className={appStyle.title}>Login</div>
+        <div className={appStyle.row}>
+          <div className={appStyle.col_100}>
+            There are 2 options for logging in:
+            <ul>
+              <li>Connect to a Mobile Wallet using Wallet Connect and a QRCode</li>
+              <li>Connect to Meta Mask Browser Wallet</li>
+            </ul>
+            When you click an option it will open up in a new tab and connect your wallet to
+            the chrome extension which looks like:
+            <p>
+              {chromeExtensionUrl}
+            </p>
+            <p>
+              Note that most browser wallets inject their proxy connection into window.ethereum
+              but window.ethereum is not visible within a chrome extension context.  Meta Mask
+              however offers a Duplex Port Stream way of connecting to it which is why we can
+              offer Meta Mask as a browser wallet connection option.
+            </p>
+          </div>
+        </div>
+
+        <div className={appStyle.row}>
+          <div className={appStyle.col_100}>
+            <button
+              className={appStyle.button}
+              onClick={openWalletConnectInNewTab}>Connect to Mobile Wallet</button>
+            <button
+              className={appStyle.button}
+              style={{ paddingLeft: "20px" }}
+              onClick={openWalletConnectInNewTab}>Connect to Meta Mask Browser Wallet</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      {getJSX()}
+    </>
   );
 }
